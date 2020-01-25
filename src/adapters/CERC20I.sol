4a0d3cfe1b906b5c9b365154c4973d72245d2434
James Foley <james.foley@protonmail.com>
2020-01-25 15:41:59 -0800

    CERC20i.sol

---
+++ src/adapters/CERC20I.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.12;
+
+
+
+interface CErc20Interface {
+    function mint(uint mintAmount) external returns (uint);
+    function redeem(uint redeemTokens) external returns (uint);
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
+    function borrow(uint borrowAmount) external returns (uint);
+    function repayBorrow(uint repayAmount) external returns (uint);
+    function repayBorrowBehalf(address borrower, uint repayAmount) external returns (uint);
+    function liquidateBorrow(address borrower, uint repayAmount, CTokenInterface cTokenCollateral) external returns (uint);
+    function getCash() external view returns (uint);
+}
+
