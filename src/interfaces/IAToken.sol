f5578fa98d4f5f5185a39cda71b7c2e34983e8f0
James Foley <james.foley@protonmail.com>
2020-04-06 19:49:18 -0700

    removed ancillary events from aave interface

--- src/interfaces/IAToken.sol
+++ src/interfaces/IAToken.sol
@@ -2,61 +2,6 @@ pragma solidity ^0.5.15;
 
 interface IAToken {
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    event Redeem(
-        address indexed _from,
-        uint256 _value,
-        uint256 _fromBalanceIncrease,
-        uint256 _fromIndex
-    );
-
-    event MintOnDeposit(
-        address indexed _from,
-        uint256 _value,
-        uint256 _fromBalanceIncrease,
-        uint256 _fromIndex
-    );
-
-    event BurnOnLiquidation(
-        address indexed _from,
-        uint256 _value,
-        uint256 _fromBalanceIncrease,
-        uint256 _fromIndex
-    );
-
-    event BalanceTransfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value,
-        uint256 _fromBalanceIncrease,
-        uint256 _toBalanceIncrease,
-        uint256 _fromIndex,
-        uint256 _toIndex
-    );
-
-    event InterestStreamRedirected(
-        address indexed _from,
-        address indexed _to,
-        uint256 _redirectedBalance,
-        uint256 _fromBalanceIncrease,
-        uint256 _fromIndex
-    );
-
-    event RedirectedBalanceUpdated(
-        address indexed _targetAddress,
-        uint256 _targetBalanceIncrease,
-        uint256 _targetIndex,
-        uint256 _redirectedBalanceAdded,
-        uint256 _redirectedBalanceRemoved
-    );
-
-    event InterestRedirectionAllowanceChanged(
-        address indexed _from,
-        address indexed _to
-    );
-
     function transfer(address recipient, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
@@ -74,6 +19,5 @@ interface IAToken {
     function getRedirectedBalance(address _user) external view returns(uint256);
     function decimals () external view returns (uint256);
     function deposit(uint256 _amount) external;
-
 
 }
