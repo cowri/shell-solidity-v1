aaa1d07174aca04668eee1e2634f9406226f31cb
realisation <realisation@protonmail.com>
2020-07-23 14:56:17 -0700

    kovan testing scripts for matching

---
+++ testKovanLoudMatch.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+source .sethrc && dapp --use solc:0.5.15 build && DAPP_TEST_NUMBER=$(seth block latest number) DAPP_TEST_TIMESTAMP=$(seth block latest timestamp) hevm dapp-test --rpc $ETH_RPC_URL --json-file=out/dapp.sol.json --match $1 --verbose 2
