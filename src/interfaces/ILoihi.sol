009084c4646ff75019c5a4cbb27d260c2f036736
realisation <realisation@protonmail.com>
2020-07-24 12:14:41 -0700

    removed extra assimilator interface and adjusted parameter in ILoihi.sol to avoid compiler shadow warning

--- src/interfaces/ILoihi.sol
+++ src/interfaces/ILoihi.sol
@@ -103,7 +103,7 @@ interface ILoihi {
     function transfer(address recipient, uint amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint amount) external returns (bool);
     function approve(address spender, uint amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint);
+    function allowance(address _owner, address _spender) external view returns (uint);
 
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
