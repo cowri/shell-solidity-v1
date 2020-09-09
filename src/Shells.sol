8ed6d8ad5937ea19e49cbb2cd005110f885688ca
realisation <realisation@protonmail.com>
2020-09-09 13:05:40 -0700

    deployed version of Shells.sol

--- src/Shells.sol
+++ src/Shells.sol
@@ -42,11 +42,9 @@ library Shells {
      * - `recipient` cannot be the zero address.
      * - the caller must have a balance of at least `amount`.
      */
-    function transfer(LoihiStorage.Shell storage shell, address recipient, uint256 amount) internal returns (bool) {
+    function transfer(LoihiStorage.Shell storage shell, address recipient, uint256 amount) external returns (bool) {
         _transfer(shell, msg.sender, recipient, amount);
-
         return true;
-
     }
 
     /**
@@ -56,11 +54,9 @@ library Shells {
      *
      * - `spender` cannot be the zero address.
      */
-    function approve(LoihiStorage.Shell storage shell, address spender, uint256 amount) internal returns (bool) {
+    function approve(LoihiStorage.Shell storage shell, address spender, uint256 amount) external returns (bool) {
         _approve(shell, msg.sender, spender, amount);
-
         return true;
-
     }
 
     /**
@@ -75,13 +71,10 @@ library Shells {
      * - the caller must have allowance for `sender`'s tokens of at least
      * `amount`
      */
-    function transferFrom(LoihiStorage.Shell storage shell, address sender, address recipient, uint256 amount) internal returns (bool) {
+    function transferFrom(LoihiStorage.Shell storage shell, address sender, address recipient, uint256 amount) external returns (bool) {
         _transfer(shell, msg.sender, recipient, amount);
-
         _approve(shell, sender, msg.sender, sub(shell.allowances[sender][msg.sender], amount, "Shell/insufficient-allowance"));
-
         return true;
-
     }
 
     /**
@@ -96,11 +89,9 @@ library Shells {
      *
      * - `spender` cannot be the zero address.
      */
-    function increaseAllowance(LoihiStorage.Shell storage shell, address spender, uint256 addedValue) internal returns (bool) {
+    function increaseAllowance(LoihiStorage.Shell storage shell, address spender, uint256 addedValue) external returns (bool) {
         _approve(shell, msg.sender, spender, add(shell.allowances[msg.sender][spender], addedValue, "Shell/approval-overflow"));
-
         return true;
-
     }
 
     /**
@@ -117,11 +108,9 @@ library Shells {
      * - `spender` must have allowance for the caller of at least
      * `subtractedValue`.
      */
-    function decreaseAllowance(LoihiStorage.Shell storage shell, address spender, uint256 subtractedValue) internal returns (bool) {
+    function decreaseAllowance(LoihiStorage.Shell storage shell, address spender, uint256 subtractedValue) external returns (bool) {
         _approve(shell, msg.sender, spender, sub(shell.allowances[msg.sender][spender], subtractedValue, "Shell/allowance-decrease-underflow"));
-
         return true;
-
     }
 
     /**
@@ -138,20 +127,13 @@ library Shells {
      * - `recipient` cannot be the zero address.
      * - `sender` must have a balance of at least `amount`.
      */
-    function _transfer(
-        LoihiStorage.Shell storage shell,
-        address sender,
-        address recipient,
-        uint256 amount
-    ) private {
-
+    function _transfer(LoihiStorage.Shell storage shell, address sender, address recipient, uint256 amount) private {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
 
         shell.balances[sender] = sub(shell.balances[sender], amount, "Shell/insufficient-balance");
         shell.balances[recipient] = add(shell.balances[recipient], amount, "Shell/transfer-overflow");
         emit Transfer(sender, recipient, amount);
-
     }
 
 
@@ -168,19 +150,12 @@ library Shells {
      * - `_owner` cannot be the zero address.
      * - `spender` cannot be the zero address.
      */
-    function _approve(
-        LoihiStorage.Shell storage shell,
-        address _owner,
-        address spender,
-        uint256 amount
-    ) private {
-
+    function _approve(LoihiStorage.Shell storage shell, address _owner, address spender, uint256 amount) private {
         require(_owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
         shell.allowances[_owner][spender] = amount;
         emit Approval(_owner, spender, amount);
-
     }
 
 }
