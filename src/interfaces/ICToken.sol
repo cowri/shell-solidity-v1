e3201021fe0ffc129d43bc77e09f36e19838fccc
realisation <realisation@protonmail.com>
2020-09-09 12:54:05 -0700

    added allowance(owner,spender) to ICToken.sol

--- src/interfaces/ICToken.sol
+++ src/interfaces/ICToken.sol
@@ -15,6 +15,7 @@ interface ICToken {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function balanceOfUnderlying(address account) external returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
