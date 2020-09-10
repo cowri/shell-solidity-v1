2dd86d9c2f151c927c4335c6c215def3a4891b06
realisation <realisation@protonmail.com>
2020-09-10 16:06:18 -0700

    Adjusted viewability of Orchestrator.initialize and other minor things

--- src/Orchestrator.sol
+++ src/Orchestrator.sol
@@ -43,8 +43,6 @@ library Orchestrator {
         uint256 _feeAtHalt,
         uint256 _epsilon,
         uint256 _lambda
-    // ) external returns (
-        // uint256 max_
     ) external {
 
         require(_alpha < 1e18 && _alpha > 0, "Shell/parameter-invalid-alpha");
@@ -70,8 +68,6 @@ library Orchestrator {
         shell.omega = getNewOmega(shell);
 
         emit ParametersSet(_alpha, _beta, shell.delta.mulu(1e18), _epsilon, _lambda, shell.omega.mulu(1e18));
-
-        // max_ = _feeAtHalt;
 
     }
 
@@ -105,10 +101,10 @@ library Orchestrator {
         address[] storage numeraires,
         address[] storage reserves,
         address[] storage derivatives,
-        address[] memory _assets,
-        uint[] memory _assetWeights,
-        address[] memory _derivativeAssimilators
-    ) public {
+        address[] calldata _assets,
+        uint[] calldata _assetWeights,
+        address[] calldata _derivativeAssimilators
+    ) external {
         
         for (uint i = 0; i < _assetWeights.length; i++) {
 
@@ -264,7 +260,9 @@ library Orchestrator {
 
     }
 
-    function viewShell (LoihiStorage.Shell storage shell) external view returns (
+    function viewShell (
+        LoihiStorage.Shell storage shell
+    ) external view returns (
         uint alpha_,
         uint beta_,
         uint delta_,
