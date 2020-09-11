92bd932b9ba4161a167392ce10499136198b2629
realisation <realisation@protonmail.com>
2020-09-10 19:05:55 -0700

    updated proportional liq tests

--- src/test/deposits/depositsTemplate.sol
+++ src/test/deposits/depositsTemplate.sol
@@ -474,18 +474,9 @@ contract SelectiveDepositTemplate is Setup {
             address(susd), 50e18
         );
 
-        ( , uint256[] memory _before ) = l.liquidity();
+        ( uint shellsMinted, uint[] memory deposits ) = l.proportionalDeposit(90e18, 1e50);
 
-        l.proportionalDeposit(90e18, 1e50);
-
-        ( , uint256[] memory after_ ) = l.liquidity();
-
-        after_[0] = after_[0] - _before[0];
-        after_[1] = after_[1] - _before[1];
-        after_[2] = after_[2] - _before[2];
-        after_[3] = after_[3] - _before[3];
-
-        return after_;
+        return deposits;
 
     }
 
@@ -498,18 +489,9 @@ contract SelectiveDepositTemplate is Setup {
             address(susd), 30e18
         );
 
-        ( , uint256[] memory _before ) = l.liquidity();
+        ( uint shellsMinted, uint[] memory deposits ) = l.proportionalDeposit(90e18, 1e50);
 
-        l.proportionalDeposit(90e18, 1e50);
-
-        ( , uint256[] memory after_ ) = l.liquidity();
-
-        after_[0] = after_[0] - _before[0];
-        after_[1] = after_[1] - _before[1];
-        after_[2] = after_[2] - _before[2];
-        after_[3] = after_[3] - _before[3];
-
-        return after_;
+        return deposits;
 
     }
 
@@ -521,19 +503,10 @@ contract SelectiveDepositTemplate is Setup {
             address(usdt), 55e6,
             address(susd), 30e18
         );
+        
+        ( uint shellsMinted, uint[] memory deposits ) = l.proportionalDeposit(90e18, 1e50);
 
-        ( , uint256[] memory _before ) = l.liquidity();
-
-        l.proportionalDeposit(90e18, 1e50);
-
-        ( , uint256[] memory after_ ) = l.liquidity();
-
-        after_[0] = after_[0] - _before[0];
-        after_[1] = after_[1] - _before[1];
-        after_[2] = after_[2] - _before[2];
-        after_[3] = after_[3] - _before[3];
-
-        return after_;
+        return deposits;
 
     }
 
