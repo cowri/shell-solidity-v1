e49d4c7189e23a301930211a1b8f69f59381fc96
realisation <realisation@protonmail.com>
2020-09-11 17:11:13 -0700

    added assimilator test harness in debug test

--- src/test/debug.t.sol
+++ src/test/debug.t.sol
@@ -10,36 +10,36 @@ import "./setup/methods.sol";
 
 import "abdk-libraries-solidity/ABDKMath64x64.sol";
 
+import "../Assimilators.sol";
+
+
 contract DebugTest is Setup, DSMath, DSTest {
 
     using ABDKMath64x64 for uint;
     using ABDKMath64x64 for int128;
+    
 
-    using LoihiMethods for Loihi;
+    AssimilatorBouncer bouncer;
 
-    Loihi l;
 
     event log_bytes(bytes32, bytes4);
     event log_uints(bytes32, uint256[]);
 
     function setUp() public {
 
-        l = getLoihiSuiteOne();
+        setupStablecoinsKovan();
+        setupAssimilatorsSetOneKovan();
+        
+        bouncer = new AssimilatorBouncer();
+
+        approveStablecoins(address(bouncer));
 
     }
 
     function testDebug () public {
-
-        uint256 p3 = .3e18;
-
-        int128 p3divu = p3.divu(1e18);
-
-        int128 onedivu = uint256(.25e18).divu(1e18);
-
-        emit log_named_int("int128", onedivu);
-
-
-
+        
+        bouncer.depositRaw(address(cdaiAssimilator), 300e8);
+        
     }
     
     function testMath () public {
@@ -47,6 +47,59 @@ contract DebugTest is Setup, DSMath, DSTest {
         uint256 a = 1;
 
         int128 a64 = a.fromUInt();
+
+    }
+
+}
+
+
+contract AssimilatorBouncer {
+
+    using ABDKMath64x64 for uint256;
+    using ABDKMath64x64 for int128;
+
+    constructor () public { 
+
+    }
+
+    function safeApprove (address _token, address _spender, uint256 _value) public {
+        ( bool success, bytes memory returndata ) = _token.call(abi.encodeWithSignature("approve(address,uint256)", _spender, _value));
+        require(success, "SafeERC20: low-level call failed");
+    }
+
+    function depositRaw (address _assim, uint256 _amt) public returns (int256 amt_, int256 bal_) {
+
+        ( int128 _amt64x64, int128 _bal64x64 ) = Assimilators.intakeRawAndGetBalance(_assim, _amt);
+
+        amt_ = _amt64x64.muli(1e18);
+
+        bal_ = _bal64x64.muli(1e18);
+
+    }
+
+    function depositNumeraire (address _assim, uint256 _amt) public returns (uint256 amt_) {
+
+        int128 _amt64x64 = _amt.divu(1e18);
+
+        amt_ = Assimilators.intakeNumeraire(_assim, _amt64x64);
+
+    }
+
+    function withdrawRaw (address _assim, uint256 _amt) public returns (int256 amt_, int256 bal_) {
+
+        ( int128 _amt64x64, int128 _bal64x64 ) = Assimilators.outputRawAndGetBalance(_assim, msg.sender, _amt);
+
+        amt_ = _amt64x64.muli(1e18);
+
+        bal_ = _bal64x64.muli(1e18);
+
+    }
+
+    function withdrawNumeraire (address _assim, uint256 _amt) public returns (uint256 amt_) {
+
+        int128 _amt64x64 = _amt.divu(1e18);
+
+        amt_ = Assimilators.outputNumeraire(_assim, msg.sender, _amt64x64);
 
     }
 
